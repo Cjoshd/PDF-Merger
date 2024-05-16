88cb45f58d60f749ce3998cafcce5e9f03c5f40a
Cjoshd <cjoshuadev@gmail.com>
2024-05-16 20:04:47 +0800

    added some comments

--- pdf_merger.py
+++ pdf_merger.py
@@ -4,13 +4,14 @@ import PyPDF2 # install PyPDF2 if not present: in terminal type: pip3 install Py
 # create the merger object
 merger = PyPDF2.PdfMerger()
 
-# the current  code checks every file in the directory in which this programme runs and merges all existing PDFs in the directory.
-# original PDFs will still remain.
+# the current  code checks every file in the directory in which this programme runs and merges all existing PDFs in the directory.git 
+# every original file will remain unchanged
 for file in os.listdir(os.curdir): 
     if file.endswith('.pdf'):
         with open(file, 'rb') as pdf_file:
             merger.append(file)
 
+# the merged file will be name "newfile.pdf"
 merger.write('newfile.pdf')
 merger.close()
         
